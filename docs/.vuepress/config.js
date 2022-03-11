bfd00c652019f2459db0b958c92ba41343a077ae
rich9341 <rich930401@gmail.com>
2022-03-11 15:28:10 +0800

    新的實驗

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,7 +14,8 @@ module.exports = {
   themeConfig: {
     logo: "https://i.epochtimes.com/assets/uploads/2021/08/id13156667-shutterstock_376153318-450x322.jpg",
     navbar: [...getNavBar()],
-    sidebar: { ...getSideBar() }, 
+    sidebar: [{ ...getSideBar() }
+    ], 
   },
 };
 
