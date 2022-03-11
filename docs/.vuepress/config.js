f9e697bacb1a2b38e96c9bef36911da055a00045
rich9341 <rich930401@gmail.com>
2022-03-11 15:18:56 +0800

    new

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,10 +14,7 @@ module.exports = {
   themeConfig: {
     logo: "https://i.epochtimes.com/assets/uploads/2021/08/id13156667-shutterstock_376153318-450x322.jpg",
     navbar: [...getNavBar()],
-    sidebar: [
-      'h1n1',
-
-    ],
+    sidebar: { ...getSideBar() },
   },
 };
 
