2b19f0194602e04b209de7d94046a1cd551c6f83
rich9341 <rich930401@gmail.com>
2022-03-13 18:27:00 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,40 +13,11 @@ module.exports = {
   description: "Vuepress ",
   themeConfig: {
     logo: "https://i.epochtimes.com/assets/uploads/2021/08/id13156667-shutterstock_376153318-450x322.jpg",
-    navbar: [...getNavBar()],
     sidebar: { ...getSideBar() },
   },
 };
 
-function getNavBar() {
-  const navbar = [];
-  folders.forEach((folder) => {
-    navbar.push({
-      text: folder.toUpperCase(),
-      link: `/${folder}/`,
-    });
-  });
-  return navbar;
-}
 
-function getSideBar() {
-  const sidebar = {};
-  folders.forEach((folder) => {
-    sidebar[`/${folder}/`] = [];
-    const folderFiles = fs.readdirSync(path.join(rootFolder, folder));
-    const children = [];
-    folderFiles
-      .filter((item) => item.toLowerCase() != "readme.md")
-      .forEach((file) => {
-        children.push(`/${folder}/${file}`);
-      });
-    sidebar[`/${folder}/`].push({
-      text: folder.toUpperCase(),
-      children: [`/${folder}/`, ...children],
-    });
-  });
-  return sidebar;
-}
 
 function getSideBar() {
   const sidebar = {};
